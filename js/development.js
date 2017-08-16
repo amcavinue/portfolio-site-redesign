3ac5604a1044019837983bba56452c227698d8af
Aaron <aaronmcavinue@gmail.com>
2017-08-16 19:17:17 +0000

    Files cleanup.

--- js/development.js
+++ js/development.js
@@ -1,36 +1,3 @@
-/* Match column height */
-$('.service-item').matchHeight();
-
-/* Animate to auto height: https://css-tricks.com/snippets/jquery/animate-heightwidth-to-auto/ */
-jQuery.fn.animateAuto = function(prop, speed, callback){
-  var elem, height, width;
-  return this.each(function(i, el){
-    el = jQuery(el), elem = el.clone().css({"height":"auto","width":"auto"}).appendTo("body");
-    height = elem.css("height"),
-    width = elem.css("width"),
-    elem.remove();
-    
-    if(prop === "height")
-      el.animate({"height":height}, speed, callback);
-    else if(prop === "width")
-      el.animate({"width":width}, speed, callback);
-    else if(prop === "both")
-      el.animate({"width":width,"height":height}, speed, callback);
-  });
-}
-
-function carouselHeightMatch() {
-  var maxHeight = -1;
-     $('.item').each(function() {
-       maxHeight = maxHeight > $(this).height() ? maxHeight : $(this).height();
-     });
-  
-     $('.item').each(function() {
-       $(this).height(maxHeight);
-     });
-}
-
-/* Show more fade: https://css-tricks.com/text-fade-read-more/ */
 $(function() {
   var heightIncrement = 765,
       currHeight = 765;
@@ -39,21 +6,21 @@ $(function() {
   loadPartials(function(content) {
     //Compile main template
     var template = Handlebars.compile(content.filter("#development-page").html());
-    
     var data = {
       header: {
         
       }
     };
-    
     document.body.innerHTML = template(data);
   });
-  	    
-  $('.work-samples').css({'height' : currHeight});
+  
+  /* Match column height */
+  $('.service-item').matchHeight();
   	
+	/* Show more fade: https://css-tricks.com/text-fade-read-more/ */
+	$('.work-samples').css({'height' : currHeight});
   $('.work-samples .button').click(function() {
     var scrollHeight = $('.work-samples')[0].scrollHeight;
-  	
   	if (currHeight + heightIncrement < scrollHeight) {
   	  currHeight += heightIncrement;
   	  $('.work-samples')
@@ -66,7 +33,6 @@ $(function() {
   	  });
   	  $('.show-more').fadeOut();
   	}
-  	
   	// prevent jump-down
   	return false;
   });
