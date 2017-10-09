26e14afb4bc28400b3d0636b8c7b38c57fad8e73
Aaron <aaronmcavinue@gmail.com>
2017-10-09 18:21:35 +0000

    Clean up development page logic. Start adding carousel progress bar.

--- js/pages/development.js
+++ js/pages/development.js
@@ -1,7 +1,79 @@
-$(function() {
-  var heightIncrement = 765,
-      currHeight = 765;
+function showMore(currHeight, heightIncrement) {
+	/* Show more fade: https://css-tricks.com/text-fade-read-more/ */
+	$('.work-samples').css({'height' : currHeight});
+  $('.work-samples .button').click(function() {
+    var scrollHeight = $('.work-samples')[0].scrollHeight;
+  	if (currHeight + heightIncrement < scrollHeight) {
+  	  currHeight += heightIncrement;
+  	  $('.work-samples')
+  		.animate({
+  			'height': currHeight
+  		}, 500);
+  	} else {
+  	  $('.work-samples').animateAuto('height', 500, function() {
+  	    $('.work-samples').css('height', 'auto');
+  	  });
+  	  $('.show-more').fadeOut();
+  	}
+  	// prevent jump-down
+  	return false;
+  });
+}
+
+function scrollNav() {
+  // ScrollMagic options.
+  var controller = new ScrollMagic.Controller({
+    globalSceneOptions: {
+      triggerHook: 0.65,
+      reverse: true,
+    }
+  });
   
+  controller.scrollTo(function(target) {
+    TweenMax.to(window, 0.5, {
+      scrollTo : {
+        y : target,
+        autoKill : true
+      },
+      ease : Cubic.easeInOut
+    });
+  });
+  
+  
+  $('section, footer').each(function(i, el) {
+    $('#scroll-navigation').append('<a class="scroll-link" id="scroll-link-' + i + '" href="#' + $(this).attr('id') + '">&#9679;</a>');
+    // var currHeight = $(this).height();
+    var section = this;
+	  var scene = new ScrollMagic.Scene({
+  	  triggerElement: '#' + $(this).attr('id'),
+  	  duration: function() {
+  	    return $(section).height();
+  	  }
+  	})
+			.setClassToggle('#scroll-link-' + i, "scroll-active") // add class toggle
+			// .addIndicators() // For debugging.s
+			.addTo(controller);
+  });
+  $('#scroll-navigation').show();
+  
+  $(document).on('click', '.scroll-link', function(e) {
+    var id = $(this).attr("href"); // grab the href attribute value
+  
+    if($(id).length > 0) {
+      // prevents default behavior of links.
+      e.preventDefault();
+      
+      // trigger scroll
+      controller.scrollTo(id);
+    }
+  });
+}
+
+function carouselProgress() {
+  
+}
+
+$(function() {
   // Load the Handlebars template. 
   loadPartials(function(content) {
     //Compile main template
@@ -26,72 +98,9 @@ $(function() {
     
     /* Match column height */
     $('.service-item').matchHeight();
-    	
-  	/* Show more fade: https://css-tricks.com/text-fade-read-more/ */
-  	$('.work-samples').css({'height' : currHeight});
-    $('.work-samples .button').click(function() {
-      var scrollHeight = $('.work-samples')[0].scrollHeight;
-    	if (currHeight + heightIncrement < scrollHeight) {
-    	  currHeight += heightIncrement;
-    	  $('.work-samples')
-    		.animate({
-    			'height': currHeight
-    		}, 500);
-    	} else {
-    	  $('.work-samples').animateAuto('height', 500, function() {
-    	    $('.work-samples').css('height', 'auto');
-    	  });
-    	  $('.show-more').fadeOut();
-    	}
-    	// prevent jump-down
-    	return false;
-    });
     
-    // ScrollMagic options.
-    var controller = new ScrollMagic.Controller({
-      globalSceneOptions: {
-        triggerHook: 0.7,
-        reverse: true,
-      }
-    });
-    
-    controller.scrollTo(function(target) {
-      TweenMax.to(window, 0.5, {
-        scrollTo : {
-          y : target,
-          autoKill : true
-        },
-        ease : Cubic.easeInOut
-      });
-    });
-    
-    
-    $('section, footer').each(function(i, el) {
-      $('#scroll-navigation').append('<a class="scroll-link" id="scroll-link-' + i + '" href="#' + $(this).attr('id') + '">&#9679;</a>');
-      // var currHeight = $(this).height();
-      var section = this;
-  	  var scene = new ScrollMagic.Scene({
-    	  triggerElement: '#' + $(this).attr('id'),
-    	  duration: function() {
-    	    return $(section).height();
-    	  }
-    	})
-				.setClassToggle('#scroll-link-' + i, "scroll-active") // add class toggle
-				// .addIndicators() // For debugging.s
-				.addTo(controller);
-    });
-    $('#scroll-navigation').show();
-    
-    $(document).on('click', '.scroll-link', function(e) {
-      var id = $(this).attr("href"); // grab the href attribute value
-    
-      if($(id).length > 0) {
-        // prevents default behavior of links.
-        e.preventDefault();
-        
-        // trigger scroll
-        controller.scrollTo(id);
-      }
-    });
+    showMore(765, 765);
+    scrollNav();
+    carouselProgress();
   });
 });
