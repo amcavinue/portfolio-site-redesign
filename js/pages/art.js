c0a5eba8ea01da8388e198179ffcd4623280e7bb
Aaron <aaronmcavinue@gmail.com>
2017-10-16 19:45:16 +0000

    Tweaks.

--- js/pages/art.js
+++ js/pages/art.js
@@ -4,7 +4,7 @@ function renderImages(images) {
     imageHtml +=
       '<div class="col-xs-12 image-card">' +
         '<div class="card-container">' +
-          '<div class="img-container col-md-9" data-description="' + image.filename + '">' +
+          '<div class="img-container col-md-6 col-md-offset-3" data-description="' + image.filename + '">' +
             '<span class="img-helper"></span>' +
             '<img src="../content/loading.gif" data-src="../' + image.src + '">' +
           '</div>' +
